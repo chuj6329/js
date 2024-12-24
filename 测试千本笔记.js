057efb0ad49976d525bb4a6eb336f255696ef303
chuj6329 <z199473847@gmail.com>
2024-12-24 22:33:41 +0800

    创建 测试千本笔记.js

---
+++ 测试千本笔记.js
@@ -0,0 +1,18 @@
+[
+  {
+    "token" : "05e3d724b43801a0a68980370af2d774",
+    "attributes" : {
+      "noteNumber" : 7,
+    "eventId" : "member_permanent_popup",
+    "app" : "id1514322479",
+    "appv" : "3.15.1",
+    "model" : "iPhone",
+    "appvn" : "1"
+  }
+]
+
+{
+  "ret" : 200,
+  "msg" : "1738462947",
+  "code" : 200
+}
