fc74743c3902e4154f185badf18de3a73e7c7c33
chuj6329 <z199473847@gmail.com>
2025-04-01 20:30:45 +0800

    更新 test.js

--- test.js
+++ test.js
@@ -13,17 +13,17 @@ hostname = api.swiftgram.app
 
 let obj = JSON.parse($response.body);
 
-// 修改 `announcements_data` 以移除 Swiftgram Pro 订阅提醒
+
 if (obj.global) {
-    obj.global.announcements_data = "[]"; // 清空公告
-    obj.global.payments_enabled = false; // 禁用付款提示
-    obj.global.can_grant = true; // 允许解锁 Pro
+    obj.global.announcements_data = "[]"; 
+    obj.global.payments_enabled = false; 
+    obj.global.can_grant = true; 
 }
 
-// 直接赋值 Pro 功能
+
 obj.user = obj.user || {};
-obj.user.is_pro = true;  // 设定 Pro 状态
-obj.user.can_buy_in_beta = true;  // 允许购买 Beta 版本
-obj.user.can_send_telescope = true; // 允许 Telescope 发送功能
+obj.user.is_pro = true;  
+obj.user.can_buy_in_beta = true;  
+obj.user.can_send_telescope = true; 
 
 $done({ body: JSON.stringify(obj) });
