b99fdeb85decc9b3b104a912d9d1f10c0c42498a
chuj6329 <z199473847@gmail.com>
2025-04-06 14:26:52 +0800

    更新 test.js

--- test.js
+++ test.js
@@ -10,48 +10,33 @@ hostname = api.revenuecat.com
 *
 */
 
-const response = {
-  request_date_ms: 1743919753314,
-  request_date: "2025-04-06T06:09:13Z",
-  subscriber: {
-    non_subscriptions: {},
-    first_seen: "2025-04-06T06:09:13Z",
-    original_application_version: null,
-    other_purchases: {},
-    management_url: null,
-    subscriptions: {},
-    entitlements: {},
-    original_purchase_date: true,
-    original_app_user_id: "$RCAnonymousID:e976d945222e460aa3db76836fa4cbdd",
-    last_seen: "2025-04-06T06:09:13Z"
+
+
+let obj = JSON.parse($response.body);
+
+
+const productId = "your.product.id";
+const entitlementId = "pro";
+
+obj.subscriber.entitlements = {
+  [entitlementId]: {
+    expires_date: null,
+    product_identifier: productId,
+    purchase_date: "2025-04-06T06:09:13Z"
   }
 };
 
-
-const modifiedResponse = {
-  ...response,
-  subscriber: {
-    ...response.subscriber,
-    entitlements: {
-      pro: {
-        expires_date: null,
-        product_identifier: "your.product.id",
-        purchase_date: "2025-04-06T06:09:13Z"
-      }
-    },
-    subscriptions: {
-      "your.product.id": {
-        billing_issues_detected_at: null,
-        expires_date: null,
-        is_sandbox: false,
-        original_purchase_date: "2025-04-06T06:09:13Z",
-        period_type: "active",
-        purchase_date: "2025-04-06T06:09:13Z",
-        store: "app_store",
-        unsubscribe_detected_at: null
-      }
-    }
+obj.subscriber.subscriptions = {
+  [productId]: {
+    billing_issues_detected_at: null,
+    expires_date: null,
+    is_sandbox: false,
+    original_purchase_date: "2025-04-06T06:09:13Z",
+    period_type: "normal",
+    purchase_date: "2025-04-06T06:09:13Z",
+    store: "app_store",
+    unsubscribe_detected_at: null
   }
 };
 
-$done({ body: JSON.stringify(modifiedResponse) });
+$done({ body: JSON.stringify(obj) });
